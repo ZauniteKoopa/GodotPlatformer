4059f626867ec35796cfa6ebf096ba68a96ef193
Anthony Hoang <anthonyhoang3074@gmail.com>
2024-01-15 10:24:09 -0600

    Implemented simple camera system with springarm

--- game/Scripts/CameraSystem.gd
+++ game/Scripts/CameraSystem.gd
@@ -1,13 +1,25 @@
 extends Node3D
 
-const ROTATION_SPEED = 2;
+const MOUSE_SENSITIVITY = 0.003;
+const X_ROTATION_LIMIT = 45.0;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var cameraRotationAxis = Input.get_axis(
-		"rotate_camera_left",
-		"rotated_camera_right"
-	)
-	
-	rotate(Vector3.UP, ROTATION_SPEED * cameraRotationAxis * delta)
+# On game ready
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# main input function to handle mouse events
+func _input(event):
+	if event is InputEventMouseMotion:
+		var mouseDelta = event.relative
+		
+		# horizontal rotation
+		var yEulerAngle = transform.basis.get_euler().y + (MOUSE_SENSITIVITY * -mouseDelta.x)
+		
+		# vertical rotation
+		var xEulerAngle = transform.basis.get_euler().x + (MOUSE_SENSITIVITY * -mouseDelta.y)
+		xEulerAngle = clamp(xEulerAngle, deg_to_rad(-X_ROTATION_LIMIT), deg_to_rad(X_ROTATION_LIMIT))
+		
+		# apply rotation
+		transform.basis = transform.basis.from_euler(Vector3(xEulerAngle, yEulerAngle, 0))
+		pass
 	pass
